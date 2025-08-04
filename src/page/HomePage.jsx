a651b497094aa06c815bdeaa18e38251cac0784b
Paul2000-2000 <alexapaul2000@yahoo.com>
2025-08-04 11:55:15 +0300

    beginning of the project, navbar done

---
+++ src/page/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Navbar from '../components/navbar/Navbar'
+import Hero from '../components/hero/Hero'
+
+const HomePage = () => {
+  return (
+    <div>
+        
+        <Navbar />
+        <Hero />
+        
+    </div>
+  )
+}
+
+export default HomePage
